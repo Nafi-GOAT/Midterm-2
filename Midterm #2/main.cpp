0183310ed08e1cf7971f13898a8875c78fdfb138
Nafi-GOAT <nafih2989@gmail.com>
2025-10-16 19:15:33 -0700

    66

--- Midterm #2/main.cpp
+++ Midterm #2/main.cpp
@@ -68,91 +68,8 @@ public:
         delete temp;
     }
     
-
-    
-    void insert_after(string value, int position) {
-        if (position < 0) {
-            cout << "Position must be >= 0." << endl;
-            return;
-        }
-        Node* newNode = new Node(value);
-        if (!head) {
-            head = tail = newNode;
-            return;
-        }
-        Node* temp = head;
-        for (int i = 0; i < position && temp; ++i)
-            temp = temp->next;
-        if (!temp) {
-            cout << "Position exceeds list size. Node not inserted.\n";
-            delete newNode;
-            return;
-        }
-        newNode->next = temp->next;
-        newNode->prev = temp;
-        if (temp->next)
-            temp->next->prev = newNode;
-        else
-            tail = newNode;
-        temp->next = newNode;
-    }
-
-    void delete_val(string value) {
-        if (!head) return;
-        Node* temp = head;
-        while (temp && temp->data != value)
-            temp = temp->next;
-        if (!temp) return;
-        if (temp->prev)
-            temp->prev->next = temp->next;
-        else
-            head = temp->next;
-        if (temp->next)
-            temp->next->prev = temp->prev;
-        else
-            tail = temp->prev;
-        delete temp;
-    }
-
-    void delete_pos(int pos) {
-        if (!head) {
-            cout << "List is empty." << endl;
-            return;
-        }
-        if (pos == 1) {
-            pop_front();
-            return;
-        }
-        Node* temp = head;
-        for (int i = 1; i < pos; i++) {
-            if (!temp) {
-                cout << "Position doesn't exist." << endl;
-                return;
-            } else
-                temp = temp->next;
-        }
-        if (!temp) {
-            cout << "Position doesn't exist." << endl;
-            return;
-        }
-        if (!temp->next) {
-            pop_back();
-            return;
-        }
-        Node* tempPrev = temp->prev;
-        tempPrev->next = temp->next;
-        temp->next->prev = tempPrev;
-        delete temp;
-    }
-
-
-
-
     void pop_back() {
-        if (!tail) {
-            cout << "List is empty." << endl;
-            return;
-        }
+        if (!tail) return;
         Node* temp = tail;
         if (tail->prev) {
             tail = tail->prev;
@@ -160,14 +77,6 @@ public:
         } else
             head = tail = nullptr;
         delete temp;
-    }
-
-    ~DoublyLinkedList() {
-        while (head) {
-            Node* temp = head;
-            head = head->next;
-            delete temp;
-        }
     }
 
     void print() {
