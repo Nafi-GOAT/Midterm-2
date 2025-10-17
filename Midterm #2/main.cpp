6a6844487f1d055f10dd1a921b2048f19700550a
Nafi-GOAT <nafih2989@gmail.com>
2025-10-16 18:36:11 -0700

    Initial Commit

---
+++ Midterm #2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Midterm #2
+//
+//  Created by Nafi on 10/16/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
